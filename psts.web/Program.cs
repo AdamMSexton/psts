c5a7c4404c783ca026c2da12f7b9ba69e5fa44b6
Adam M. Sexton <AdamMSexton@Live.com>
2026-01-19 16:28:59 -0500

    EF Core wired in.  Successfully setup DB migrations to PostgresDB.

--- psts.web/Program.cs
+++ psts.web/Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Psts.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// AMS - Reqister EF Core
+builder.Services.AddDbContext<PstsDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
 var app = builder.Build();
+
+// AMS - Apply migrations (for DB init and validation)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PstsDbContext>();
+    db.Database.Migrate();
+}
 
 app.MapGet("/", () => "Hello World!");
 
