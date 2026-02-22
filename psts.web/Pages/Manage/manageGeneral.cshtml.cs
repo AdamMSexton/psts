7580731eaea3c0d0582c98c45b13a7f867bac739
eman <eespiti@jacksonville.edu>
2026-02-22 10:23:44 -0500

    manage general uopdated with forms for short code

--- psts.web/Pages/Manage/manageGeneral.cshtml.cs
+++ psts.web/Pages/Manage/manageGeneral.cshtml.cs
@@ -5,8 +5,37 @@ namespace psts.web.Pages.Manage
 {
     public class IndexModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public string? Shortcode { get; set; }
+
+        [BindProperty]
+        public string? RecordType { get; set; }
+
+        [BindProperty]
+        public string? ClientName { get; set; }
+
+        [BindProperty]
+        public string? ProjectName { get; set; }
+
+        [BindProperty]
+        public string? TaskName { get; set; }
+
+        public string? Message { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            Message = $"Shortcode '{Shortcode}' created (not yet implemented).";
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            Message = $"'{RecordType}' submitted/updated (not yet implemented).";
+            return Page();
         }
     }
 }
